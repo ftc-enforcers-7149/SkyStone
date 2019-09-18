1d1fa2033ac15d4d9a026ea1a09d5fa9bcb48987
Nathanael Gaw <nategaw@gmail.com>
2019-09-18 19:51:11 -0400

    started coding

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CollisionAvoidance/CollisionData.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CollisionAvoidance/CollisionData.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.CollisionAvoidance;
 
-public class CollisionData  {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class CollisionData extends OpMode {
+
+    //DC Motors
+
+
+    //Distance Sensors
+
+
+    public void init() {
+
+    }
+
+    public void loop() {
+
+    }
+
+    public void stop() {
+
+    }
+
 }
