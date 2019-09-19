13f1f7921ed1d85ee635c03347eacc5a83c1ad75
Nathanael Gaw <nategaw@gmail.com>
2019-09-18 21:02:38 -0400

    finished coding for Dc motors and now I have to do is sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CollisionAvoidance/CollisionData.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CollisionAvoidance/CollisionData.java
@@ -1,21 +1,25 @@
 package org.firstinspires.ftc.teamcode.CollisionAvoidance;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class CollisionData extends OpMode {
 
     //DC Motors
-
+DcMotor Motor1; //Left motor
+DcMotor Motor2; //right motor
 
     //Distance Sensors
 
 
-    public void init() {
-
+    public void loop() {
+        Motor1.setPower(gamepad1.left_stick_x);
+        Motor2.setPower(-gamepad1.left_stick_x);
     }
 
-    public void loop() {
-
+    public void init() {
+        Motor1 = hardwareMap.dcMotor.get("m1");
+        Motor2 = hardwareMap.dcMotor.get("m2");
     }
 
     public void stop() {
