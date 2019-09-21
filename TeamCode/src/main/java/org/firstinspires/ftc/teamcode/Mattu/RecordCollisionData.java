973169fef3d6e56399ccf97bdfb503b94a00a9e4
MathMonster25 <mtt2005mrc2006@gmail.com>
2019-09-21 10:52:09 -0400

    Using time instead of iterations.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mattu/RecordCollisionData.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mattu/RecordCollisionData.java
@@ -12,62 +12,66 @@ import java.util.Arrays;
 public class RecordCollisionData extends OpMode {
 
     //Array used to hold power and distance values for telemetry
-    public double[][] dataPoints;
+    public double[] dataPoints;
 
     //Power set to the motors (in this case front left)
-    public double powerFL;
+    public double power;
 
     //Distance read from front facing sensor
     public double distF;
+
+    //Time that test starts
+    public double startTime;
 
     //Gamepad inputs for changing tests
     public boolean buttonA, bumperL, bumperR;
 
     //loops is used to count the amount of loops the program has run through while testing
     //count is used to set the next index in datapoints every some amount of loops
-    public int loops, count;
+    public int count;
 
     //Used to determine when test has ended
     public boolean pause;
 
     public void init() {
         //double array that can hold 100 x 2 values
-        dataPoints = new double[100][2];
+        dataPoints = new double[20];
 
         //Initialize variables
         pause = false;
         count = 0;
-        loops = 0;
+    }
+
+    public void start() {
+        startTime = System.currentTimeMillis();
     }
 
     public void loop() {
         //General telemetry
         telemetry.addData("Done? ", pause);
-        telemetry.addData("Power:", powerFL);
-        telemetry.addData("Data Points [distance, power]: ", Arrays.toString(dataPoints));
+        telemetry.addData("Power:", power);
+        telemetry.addData("Data Points (distance per second): ", Arrays.toString(dataPoints));
 
-        if (loops > 1000) {  //If loops is within testing limit
+        if (count > 10) {  //If seconds are within testing limit
             pause = true;
-            loops = 0;
+            count = 0;
         }
         else if (pause) {      //If test completed
             if (bumperL) {
-                powerFL -= 0.1;
+                power -= 0.1;
             }                       //Adjust motor powers for next test
             else if (bumperR) {
-                powerFL += 0.1;
+                power += 0.1;
             }
             else if (buttonA) {     //Wait until button A is pressed to continue
                 pause = false;
+                startTime = System.currentTimeMillis(); //Reset startTime
             }
         }
         else {
-            count = loops % 10 == 0 ? loops / 10 : count;   //Add to count every 10 loops
+            count = (int) Math.floor((System.currentTimeMillis() - startTime) / 1000);   //Set count to amount of seconds since startTime
 
-            dataPoints[count][0] = distF;       //Set data points for distance and motor power while test is running
-            dataPoints[count][1] = powerFL;
-
-            loops++;        //Increase loops at end of testing loop
+            dataPoints[count] = distF;       //Set data point for distance at some amount of seconds
         }
     }
 }
