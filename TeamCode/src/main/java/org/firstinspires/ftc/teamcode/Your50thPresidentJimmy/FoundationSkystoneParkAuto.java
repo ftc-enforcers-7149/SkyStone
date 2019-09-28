ecef52047d2e40682c3078e2a99480b32d0c15b5
Funkeyzebra10 <adameager3@gmail.com>
2019-09-28 13:45:53 -0400

    switched to object oriented programming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Your50thPresidentJimmy/FoundationSkystoneParkAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Your50thPresidentJimmy/FoundationSkystoneParkAuto.java
@@ -1,43 +1,102 @@
 package org.firstinspires.ftc.teamcode.Your50thPresidentJimmy;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.teamcode.SkyStonev1_1;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.teamcode.Subsystems.DriveTrain;
 
-        @Autonomous(name="auto 3 Skystone")
-        public class FoundationSkystoneParkAuto extends SkyStonev1_1 {
-            DcMotor fLeft, fRight, bLeft, bRight;
-            DistanceSensor distanceL, distanceR, distanceC;
-            BNO055IMU imu;
-            Orientation angles;
+@Autonomous(name = "auto 3 Skystone")
+public class FoundationSkystoneParkAuto extends OpMode {
+    public DcMotor fLeft, fRight, bLeft, bRight;
+    int step=0;
+    DriveTrain driveTrain;
 
-            int step=0;
-            public void init() {
-                super.init();
+    BNO055IMU imu;
+    Orientation angles;
+    public void init() {
+        //Hardware mapping of the four motors
+        fLeft = hardwareMap.dcMotor.get("fLeft");
+        fRight = hardwareMap.dcMotor.get("fRight");
+        bLeft = hardwareMap.dcMotor.get("bLeft");
+        bRight = hardwareMap.dcMotor.get("bRight");
+
+        fLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        fRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        bRight.setDirection(DcMotor.Direction.FORWARD);
+        bLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        fLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //Set up imu parameters
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        telemetry.addAction(new Runnable() {
+            @Override
+            public void run() {
+                // Acquiring the angles is relatively expensive; we don't want
+                // to do that in each of the three items that need that info, as that's
+                // three times the necessary expense.
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
             }
+        });
 
-            public void loop() {
-                switch(step){
-                    //Change speed initially to make sure capstone does not dislocate
-                    case 0:driveStraight("backward", 50);
-                        break;
-                    case 1:driveStraight("forward", 24.5);
-                        break;
-                    case 2: Rotation (270);
-                        break;
-                    case 3:driveStraight("forward", 56);
-                        break;
-                    case 4:driveStraight("backward", 33);
-                        break;
-                }
-                step++;
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
 
-            }
-            public void stop() {
-                super.stop();
-            }
+        // Start the logging of measured acceleration
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+    }
+    public void start(){
+        driveTrain=new DriveTrain(bLeft,bRight,fLeft,fRight,angles);
+
+    }
+    public void loop() {
+        switch (step) {
+            //Change speed initially to make sure capstone does not dislocate
+            case 0:
+                driveTrain.driveStraight("backward", 50);
+                break;
+            case 1:
+                driveTrain.driveStraight("forward", 24.5);
+                break;
+            case 2:
+                driveTrain.Rotation(270);
+                break;
+            case 3:
+                driveTrain.driveStraight("forward", 56);
+                break;
+            case 4:
+                driveTrain.driveStraight("backward", 33);
+                break;
         }
+        step++;
+
+    }
+
+    public void stop() {
+        super.stop();
+    }
+}
