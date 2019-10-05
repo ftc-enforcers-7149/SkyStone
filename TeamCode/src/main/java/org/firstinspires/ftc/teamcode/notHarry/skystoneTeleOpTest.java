5cd0444f396b55052021bd9ca12e917685da0965
Funkeyzebra10 <adameager3@gmail.com>
2019-10-05 14:18:50 -0400

    tweaked servo test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notHarry/skystoneTeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notHarry/skystoneTeleOpTest.java
@@ -5,10 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TeleOpTest")
 public class skystoneTeleOpTest extends OpMode {
-    CRServo lArm, rArm, lGrab, rGrab;
+    Servo lArm, rArm, lGrab, rGrab;
     DcMotor fRight,fLeft,bRight,bLeft;
 
     boolean armUp, armDown;
@@ -16,10 +17,10 @@ public class skystoneTeleOpTest extends OpMode {
     float lDrive,rDrive;
     public void init(){
         //Servos
-        lArm = hardwareMap.crservo.get("lArm");
-        rArm = hardwareMap.crservo.get("rArm");
-        lGrab = hardwareMap.crservo.get("lGrab");
-        rGrab = hardwareMap.crservo.get("rGrab");
+        lArm = hardwareMap.servo.get("lArm");
+        rArm = hardwareMap.servo.get("rArm");
+        lGrab = hardwareMap.servo.get("lGrab");
+        rGrab = hardwareMap.servo.get("rGrab");
         //Drive motors
         fLeft = hardwareMap.dcMotor.get("fLeft");
         fRight = hardwareMap.dcMotor.get("fRight");
@@ -30,6 +31,11 @@ public class skystoneTeleOpTest extends OpMode {
         fRight.setDirection(DcMotorSimple.Direction.FORWARD);
         bRight.setDirection(DcMotorSimple.Direction.FORWARD);
         bLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        lArm.setDirection(Servo.Direction.REVERSE);
+        rArm.setDirection(Servo.Direction.FORWARD);
+        lGrab.setDirection(Servo.Direction.REVERSE);
+        rGrab.setDirection(Servo.Direction.FORWARD);
     }//
     public void loop(){
         armUp = gamepad1.b;
@@ -39,31 +45,19 @@ public class skystoneTeleOpTest extends OpMode {
         lDrive = gamepad1.left_trigger;
         rDrive = gamepad1.right_trigger;
 
-
-        if (armUp = true) {
-            lArm.setPower(1);
-            rArm.setPower(0);
-        } else if (armDown = true) {
-            lArm.setPower(0);
-            rArm.setPower(1);
-        } else if (gGrab = true) {
-            lGrab.setPower(1);
-            rGrab.setPower(0);
-        } else if (gRelease = true) {
-            lGrab.setPower(0);
-            rGrab.setPower(1);
+        //grab down, arm release
+        if (armUp) {
+            lArm.setPosition(0.5);
+            rArm.setPosition(0.5);
+            lGrab.setPosition(0.2);
+            rGrab.setPosition(0.2);
+        } //grab up, arm grab
+        else  {
+            lArm.setPosition(0);
+            rArm.setPosition(0);
+            lGrab.setPosition(0);
+            rGrab.setPosition(0);
         }
-        if (lDrive > 0.1){
-            bLeft.setPower((lDrive*100)/128);
-            fLeft.setPower((lDrive*100)/128);
-        }else if (rDrive > 0.1) {
-            bRight.setPower((rDrive*100)/128);
-            fRight.setPower((rDrive*100)/128);
-        }else {
-            stop();
-        }
-
-
 
     }
     public void stop(){
