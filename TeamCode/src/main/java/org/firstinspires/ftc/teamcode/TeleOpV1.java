abd9302713137d2f65cd76005185b92fcf2ea0ba
Funkeyzebra10 <adameager3@gmail.com>
2019-10-11 19:28:50 -0400

    made officail teleOp and finished claw and grabber

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpV1.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+
 @TeleOp(name = "TeleOpV1")
 public class TeleOpV1 extends OpMode {
     Servo lArm, rArm, lGrab, rGrab;
