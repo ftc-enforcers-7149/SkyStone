a3d2e0cae949668a805cd9b508749af25a0edac1
Nathanael Gaw <nategaw@gmail.com>
2019-10-14 16:35:07 -0400

    cleaned my folder and tried to modify the MechDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NateTheGrate/MechDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NateTheGrate/MechDrive.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "MechDrive")
-public class MechDrive extends OpMode {
+public class    MechDrive extends OpMode {
 
 
     DcMotor fRight, fLeft, bLeft, bRight;
