ae1c7f4c6b4870ee954f1c9a47de102b374188dd
Funkeyzebra10 <adameager3@gmail.com>
2019-11-29 10:24:43 -0500

    pushing parent init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ParentInit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ParentInit.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Func;
@@ -23,37 +25,29 @@ import java.util.Locale;
  * class used as the init that all files can inherit
  */
 public class ParentInit extends OpMode {
-    //Drive train
-    Headless driveSystem;
-
-    //Hardware
-    Servo fLFound, fRFound, bLFound, bRFound;
     Servo lArm, rArm, lGrab, rGrab;
-    DcMotor fRight,fLeft,bRight,bLeft, lift;
+    Servo fLFound, fRFound, bLFound, bRFound;
+    DcMotor fRight,fLeft,bRight,bLeft,lift;
 
-    float armUp;
-    boolean isBreak=false;
-    float liftUp,liftDown;
-    boolean lFoundationDown, rFoundationDown;
-    float grab;
+    Webcam webcam;
+    DriveTrain driveTrain;
+    FoundationV2 foundation;
+    Claw claw;
+
+    //Distance Sensors
+    DistanceSensor distanceL, distanceR, distanceC;
+    ColorSensor color;
 
     public void init(){
         //Servos
-        fLFound = hardwareMap.servo.get("fLFound");
-        fRFound = hardwareMap.servo.get("fRFound");
-        bLFound = hardwareMap.servo.get("bLFound");
-        bRFound = hardwareMap.servo.get("bRFound");
         lArm = hardwareMap.servo.get("lArm");
         rArm = hardwareMap.servo.get("rArm");
         lGrab = hardwareMap.servo.get("lGrab");
         rGrab = hardwareMap.servo.get("rGrab");
-
-        //Inits to combat lag
-        /*colorSensor = hardwareMap.colorSensor.get("color");
-        distL = hardwareMap.get(DistanceSensor.class, "distanceL");
-        distR = hardwareMap.get(DistanceSensor.class, "distanceR");
-        distC = hardwareMap.get(DistanceSensor.class, "distanceC");*/
-
+        fLFound = hardwareMap.servo.get("fLFound");
+        fRFound = hardwareMap.servo.get("fRFound");
+        bLFound = hardwareMap.servo.get("bLFound");
+        bRFound = hardwareMap.servo.get("bRFound");
         //Drive motors
         fLeft = hardwareMap.dcMotor.get("fLeft");
         fRight = hardwareMap.dcMotor.get("fRight");
@@ -61,29 +55,52 @@ public class ParentInit extends OpMode {
         bRight = hardwareMap.dcMotor.get("bRight");
         lift = hardwareMap.dcMotor.get("lift");
 
-        //Motor directions
-        fLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        fRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        bRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        bLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        distanceC = hardwareMap.get(DistanceSensor.class, "distanceC");
+        distanceL = hardwareMap.get(DistanceSensor.class, "distanceL");
+        distanceR = hardwareMap.get(DistanceSensor.class, "distanceR");
+
+        color = hardwareMap.get(ColorSensor.class, "color");
+
+        //direction of motors
+        fLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        fRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        bRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        bLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         lift.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        //Initialize drive train
-        driveSystem = new Headless(hardwareMap, telemetry, fLeft, fRight, bLeft, bRight);
-
-        //Servo directions
+        //direction of servos
+        lArm.setDirection(Servo.Direction.FORWARD);
+        rArm.setDirection(Servo.Direction.REVERSE);
+        lGrab.setDirection(Servo.Direction.REVERSE);
+        rGrab.setDirection(Servo.Direction.FORWARD);
         fLFound.setDirection(Servo.Direction.REVERSE);
         fRFound.setDirection(Servo.Direction.FORWARD);
         bLFound.setDirection(Servo.Direction.FORWARD);
         bRFound.setDirection(Servo.Direction.REVERSE);
 
-        lArm.setDirection(Servo.Direction.FORWARD);
-        rArm.setDirection(Servo.Direction.REVERSE);
-        lGrab.setDirection(Servo.Direction.REVERSE);
-        rGrab.setDirection(Servo.Direction.FORWARD);
+        lArm.setPosition(0.95);
+        rArm.setPosition(0.81);
 
-        //Lift brake
+        fLFound.setPosition(1);
+        bLFound.setPosition(1);
+
+        fRFound.setPosition(1);
+        bRFound.setPosition(1);
+
         lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.addData("sensor:",distanceC.getDeviceName());
+
+        webcam=new Webcam(hardwareMap);
+    }
+
+    public void start(){
+        driveTrain=new DriveTrain(hardwareMap,telemetry,fLeft,fRight,bLeft,bRight);
+        foundation =new FoundationV2(fLFound,fRFound,bLFound,bRFound);
+        claw=new Claw(lArm,rArm,lGrab,rGrab);
     }
 
     public void loop(){}
@@ -96,71 +113,5 @@ public class ParentInit extends OpMode {
     }
 
 
-    //configures telemetry for imu
-    public void composeTelemetry() {
 
-        // At the beginning of each telemetry update, grab a bunch of data
-        // from the IMU that we will then display in separate lines.
-        telemetry.addAction(new Runnable() { @Override public void run()
-        {
-            // Acquiring the angles is relatively expensive; we don't want
-            // to do that in each of the three items that need that info, as that's
-            // three times the necessary expense.
-            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-            gravity  = imu.getGravity();
-        }
-        });
-
-        telemetry.addLine()
-                .addData("status", new Func<String>() {
-                    @Override public String value() {
-                        return imu.getSystemStatus().toShortString();
-                    }
-                })
-                .addData("calib", new Func<String>() {
-                    @Override public String value() {
-                        return imu.getCalibrationStatus().toString();
-                    }
-                });
-
-        telemetry.addLine()
-                .addData("heading", new Func<String>() {
-                    @Override public String value() {
-                        return formatAngle(angles.angleUnit, angles.firstAngle);
-                    }
-                })
-                .addData("roll", new Func<String>() {
-                    @Override public String value() {
-                        return formatAngle(angles.angleUnit, angles.secondAngle);
-                    }
-                })
-                .addData("pitch", new Func<String>() {
-                    @Override public String value() {
-                        return formatAngle(angles.angleUnit, angles.thirdAngle);
-                    }
-                });
-
-        telemetry.addLine()
-                .addData("grvty", new Func<String>() {
-                    @Override public String value() {
-                        return gravity.toString();
-                    }
-                })
-                .addData("mag", new Func<String>() {
-                    @Override public String value() {
-                        return String.format(Locale.getDefault(), "%.3f",
-                                Math.sqrt(gravity.xAccel*gravity.xAccel
-                                        + gravity.yAccel*gravity.yAccel
-                                        + gravity.zAccel*gravity.zAccel));
-                    }
-                });
-    }
-
-    //imu formatting
-    public String formatAngle(AngleUnit angleUnit, double angle) {
-        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
-    }
-    public String formatDegrees(double degrees){
-        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
-    }
 }
