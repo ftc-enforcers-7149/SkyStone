352990ffb5f992ac0a437cd059c4ba853502a114
harry <harryhsieh03@gmail.com>
2019-11-29 11:28:40 -0500

    Worked on lift code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notHarry/towerLevelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notHarry/towerLevelTest.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
+//fish in a tube
 @TeleOp(name = "towerLevelTest")
 public class towerLevelTest extends OpMode {
     DcMotor lift;
